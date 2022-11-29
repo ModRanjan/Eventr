cedc35633f24b51a8cac353d443dc8eb888ea091
ModRanjan <ranjan.m@simformsolutions.com>
2022-11-29 17:49:04 +0530

    remove-Footer-Component

--- design-system/Organisms/Layout/index.tsx
+++ design-system/Organisms/Layout/index.tsx
@@ -29,7 +29,7 @@ const Layout = ({ children }: LayoutProps) => {
         */}
 
         {children}
-        <Footer />
+        {/* <Footer /> */}
 
         <ToastContainer
           position="top-center"

b67f0080dc14f688762815c8dee1e075a7a7b697
ModRanjan <ranjan.m@simformsolutions.com>
2022-11-29 15:41:59 +0530

    modify-and-set-layout-in-app.tsx

--- design-system/Organisms/Layout/index.tsx
+++ design-system/Organisms/Layout/index.tsx
@@ -1,23 +1,36 @@
-import React,{useState, useEffect} from 'react'
-import Footer from '../Footer';
-import Header from '../Header'
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { MenuBar } from '../../Molecules/MenuBar';
-type LayoutProps={
-    children: React.ReactNode;
-}
-const Layout = ({children}:LayoutProps) => {
-  const [render,setRender]=useState(false);
-  useEffect(() =>setRender(true),[])
-  return render && (
-    <div className="min-h-screen bg-indigo-50 my-font ">
-        <Header/>
-        <div className="hidden md:block">
-          <MenuBar />
-        </div>
+
+import { MenuBar } from '@/Molecules/MenuBar';
+
+import Header from '../Header';
+import Footer from '../Footer';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>Eventr</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="min-h-screen">
+        <Header />
+
+        {/*
+          <div className="hidden md:block"> <MenuBar /></div> 
+        */}
+
         {children}
-        <Footer/>
+        <Footer />
+
         <ToastContainer
           position="top-center"
           autoClose={5000}
@@ -28,8 +41,9 @@ const Layout = ({children}:LayoutProps) => {
           pauseOnFocusLoss
           draggable
         />
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Layout from '../design-system/Organisms/Layout';
 import { Provider } from 'react-redux';
-import store from '../redux/store';
+import store from '@/redux/store';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-   </Provider>
+    </Provider>
   );
 }
 
-export default MyApp
+export default MyApp;
